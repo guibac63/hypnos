2d23e5e77bcb3522ae1ff82349dd6a32f71ba952
guibac63 <guibac63@hotmail.fr>
2022-04-03 16:31:08 +0200

    us3:consult etablissements terminated-add pagination

--- src/Controller/EtablissementController.php
+++ src/Controller/EtablissementController.php
@@ -4,19 +4,26 @@ namespace App\Controller;
 
 
 use App\Repository\EtablissementRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EtablissementController extends AbstractController
 {
     #[Route('/etablissements', name: 'etablissement')]
-    public function index( EtablissementRepository $etb):Response
+    public function index( EtablissementRepository $etb, PaginatorInterface $paginator, Request $request):Response
     {
         $etbData = $etb->findAll();
 
+        $etbPaginated = $paginator->paginate(
+          $etbData,
+          $request->query->getInt('page',1),
+          3
+        );
 
-        return $this->render('etablissement.html.twig',['etablissements'=>$etbData]);
+        return $this->render('etablissement.html.twig',['etablissements'=>$etbPaginated]);
     }
 
 }
