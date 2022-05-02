380f9082ce802eec8170faa63e0d00dbc9a9f8df
guibac63 <guibac63@hotmail.fr>
2022-05-02 18:38:49 +0200

    add purify filter to replace raw filters in twig

--- config/bundles.php
+++ config/bundles.php
@@ -19,4 +19,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
+    Exercise\HTMLPurifierBundle\ExerciseHTMLPurifierBundle::class => ['all' => true],
 ];
